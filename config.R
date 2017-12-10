e42bcfa6750d36daa95bb527753d0e87fa0f488f
Huffyphenix <949985347@qq.com>
2017-12-10 17:48:58 +0800

    wrong name in config.R: GSACLite, change to GSCALite

--- config.R
+++ config.R
@@ -13,8 +13,8 @@ config$version <- "0.0.1"
 # It's the working directory of app
 # DEFAULT getwd()
 
-#config$wd <- "/home/liucj/github/GSACLite"
-config$wd <- "/project/huff/huff/github/GSACLite"
+#config$wd <- "/home/liucj/github/GSCALite"
+config$wd <- "/project/huff/huff/github/GSCALite"
 
 # User directory ----------------------------------------------------------
 # This controls the unique directory,
