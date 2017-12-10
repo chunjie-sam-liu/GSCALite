d0a844a4d367fa094243acbe2c1c06e202b3162b
Huffyphenix <949985347@qq.com>
2017-12-10 09:05:44 +0800

    little edit with cnv server nad rppa ui; function_ui.R hold another type of cancer type input ui.

--- server/tcga_cnv_server.R
+++ server/tcga_cnv_server.R
@@ -1,18 +1,17 @@
 # data input --------------------------------------------------------------
 # ? global data can't load here.
 # get gene set ----
-gene_set <- reactive({gene_set<-c("TP53","EZH2","CD274","CD276","CD80",
-                             "CD86","VTCN1","CD40LG","TNFRSF14",
-                             "TNFSF9","TNFSF4","CD70","CD40LG","ICOS",
-                             "BTLA","LAG3","TNFRSF9","TNFRSF4")})
+gene_set <- reactive({})
 # load cnv data ----
-# >>>>>>>>>
+cnv <- readr::read_rds(file.path(config$database, "TCGA","cnv","pancan34_cnv.rds.gz"))
+
 
 #  get cancer type --------------------------------------------------------
 cancer_type <- callModule(cancerType,"cnv")
 output$selected_cancer <- renderText(
   cancer_type()
 )
+
 
 
 # plot generation ---------------------------------------------------------
