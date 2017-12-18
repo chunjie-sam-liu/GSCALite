7e3516c6c09b061182de5a6c5a2ad0f656817252
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2017-12-18 15:40:56 +0800

    add gscalite.js for shinyjs

--- server/welcome_server.R
+++ server/welcome_server.R
@@ -1,11 +1,13 @@
 print(user_dir)
 output$output_gene_set <- shiny::renderText({
   # input <- reactiveValues("input_gene_set" = "tp53,PTeN,,hello,foo,bar,Atg5")
+  
   if (input$input_gene_set_search == 0) return()
   
   input$input_gene_set_search
   
   msg <- check_gene_set(.s = input$input_gene_set)
+  
   if (length(msg$errors > 0)) return(paste0(msg$errors, collapse = "\n"))
   
   msg <- validate_gene_set(.v = msg$gene_set, user_dir = user_dir, user_logs = user_logs)
