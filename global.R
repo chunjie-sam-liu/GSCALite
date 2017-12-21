7d00d71f76e1c7c88751fb76649e2285818025c2
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2017-12-21 20:52:51 +0800

    delete global <<<

--- global.R
+++ global.R
@@ -364,7 +364,7 @@ snv_sur_pointPlot <- function(input, output, session, data, cancer, gene, size, 
   })
 }
 
-<<<<<<< HEAD
+
 
 # snv maf summary ---------------------------------------------------------
 
@@ -396,8 +396,5 @@ snv_maf_summaryPlot <- function(input, output, session, gene_list_maf, figname) 
   }, deleteFile = TRUE)
 }
 
-snv_maf_oncoPlot <-
 
 
-=======
->>>>>>> 88e2fd93e4a3724f06b7ac778bf6c5a8481d71ef
