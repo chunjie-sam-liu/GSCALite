9694aab38057d1a1bcb814823acdf49d6c85738e
Huffyphenix <949985347@qq.com>
2017-12-26 20:29:30 +0800

    rppa data server more complete

--- server/tcga_rppa_server.R
+++ server/tcga_rppa_server.R
@@ -27,13 +27,22 @@ observeEvent(input$analysis, {
   # get gene set /cancer type data ----
   rppa_per %>%
     dplyr::filter(symbol %in% cnv_gene_list()) ->gene_list_rppa_per
+  
   # rppa global pie plot----
   gene_list_rppa_per %>%
     tidyr::unnest() %>%
     tidyr::gather(-symbol,-pathway,key="class",value="per") %>%
     dplyr::mutate(class=plyr::revalue(class,replace = c("a"="Activation","i"="Inhibition","n"="None")))->rppa_pie_plot_ready
   
-  callModule(rppaPiePlot,"rppa_pie",data=rppa_pie_plot_ready, y="per",fill="class", facet_grid=" symbol~pathway")  
+  # arugument for plot
+  rppa_pie_height <- cnv_gene_list() %>% length() *0.25
+  if(rppa_pie_height>15){rppa_pie_height <- 15}
+  if(rppa_pie_height<3){rppa_pie_height <- 3}
+  rppa_pie_outfile <- file.path(user_dir,"pngs",paste(user_id,"-","TCGA_rppa_pie_profile.png",sep=""))
+  
+  # draw ----
+  callModule(rppaPiePlot,"rppa_pie",data=rppa_pie_plot_ready, y="per",fill="class", facet_grid=" symbol~pathway", height=rppa_pie_height, outfile=rppa_pie_outfile)  
+  
   # rppa global percentage ----
   # data process
   gene_list_rppa_per %>%
@@ -42,26 +51,42 @@ observeEvent(input$analysis, {
     tidyr::gather(-symbol,-pathway,key="class",value="per") %>%
     dplyr::mutate(per=ifelse(class=="i",-per*100,per*100)) %>%
     tidyr::unite(pathway,c(pathway,class)) -> rppa_per_ready
+  
   # pic draw
-  output$rppa_per_plot <- renderPlot({
-    rppa_per_ready %>%
-      ggplot(aes(x = pathway, y = symbol))+
-      xlab("Pathway")+ylab("Symbol") +
-      guides(fill=guide_colorbar("Percent")) +
-      geom_tile(aes(fill = per), col = "white") +
-      geom_text(label=ceiling(rppa_per_ready$per)) +
-      scale_fill_gradient2(
-        high = "red",
-        mid = "white",
-        low = "blue"
-      ) +
-      theme(
-        axis.text.x = element_text(angle = 45, hjust = 1,vjust = 1),
-        axis.text.y = element_text()
-      ) + 
-      xlab("Pathway (a:activate; i:inhibit)")
-  })
+  rppa_heat_height <- cnv_gene_list() %>% length() *0.08
+  if(rppa_heat_height>15){rppa_heat_height <- 15}
+  if(rppa_heat_height<3){rppa_heat_height <- 3}
+  rppa_heat_outfile<-file.path(user_dir,"pngs",paste(user_id,"-","TCGA_rppa_heatmap_percentage.png",sep=""))
+  callModule(rppa_heat_per,"rppa_per",rppa_per_ready=rppa_per_ready, pathway="pathway",symbol="symbol", per="per", height=rppa_heat_height, outfile=rppa_heat_outfile)
+  
+  # output$rppa_per_plot <- renderImage({
+  #   rppa_per_ready %>%
+  #     ggplot(aes(x = pathway, y = symbol))+
+  #     xlab("Pathway")+ylab("Symbol") +
+  #     guides(fill=guide_colorbar("Percent")) +
+  #     geom_tile(aes(fill = per), col = "white") +
+  #     geom_text(label=ceiling(rppa_per_ready$per),size=1) +
+  #     scale_fill_gradient2(
+  #       high = "red",
+  #       mid = "white",
+  #       low = "blue"
+  #     ) +
+  #     theme(
+  #       axis.text.x = element_text(angle = 45, hjust = 1,vjust = 1),
+  #       axis.text.y = element_text()
+  #     ) +
+  #     xlab("Pathway (a:activate; i:inhibit)")->p
+  #   rppa_per_outfile <- file.path(user_dir,"pngs",paste(user_id,"-","TCGA_rppa_network_profile.png",sep=""))
+  #   
+  #   ggsave(rppa_per_outfile,p,device ="png",width =4, height = 4)
+  #   list(src = rppa_per_outfile,
+  #        contentType = 'image/png',
+  #        # width = "100%" ,
+  #        # height = 900,
+  #        alt = "This is alternate text")
+  # }, deleteFile = FALSE)
 })
+
 observeEvent(input$rppa_submit, {
 # get gene set /cancer type data ----
 
@@ -83,6 +108,9 @@ observeEvent(input$rppa_submit, {
     dplyr::filter(type=="gene") ->gene.text
   cancer_text %>%
     dplyr::filter(type=="pathway") ->path.text
+  rppa_line_height <- cnv_gene_list() %>% length() *0.1
+  if(rppa_line_height>15){rppa_line_height <- 15}
+  if(rppa_line_height<3){rppa_line_height <- 4}
   # plot draw
   output$rppa_rela_plot <- renderImage({
     ggplot() +
@@ -99,7 +127,7 @@ observeEvent(input$rppa_submit, {
         data = cancer.text, 
         mapping = aes(x = x, y = y, label = text,color=text),
         hjust = 1,
-        sieze = 2
+        size = 2
       ) +
       geom_text(
         data = gene.text, 
@@ -121,19 +149,21 @@ observeEvent(input$rppa_submit, {
         plot.margin = rep(unit(0,"null"),4),
         legend.position = "bottom",
         legend.text = element_text(size=3),
-        legend.key.size = unit(0.25,cm)
+        legend.key.size = unit(0.25,"cm"),
+        legend.title = element_text(size=4)
       ) + 
       xlab("") +
       ylab("") +
       labs(title = "Relation network between gene and cancer related pathways.") ->p
-    outfile <- paste("/project/huff/huff/github/GSCALite/userdata","/","TCGA_RPPA_rellation_network",'.png',sep="")
-    ggsave(outfile,p,device ="png",width =5, height = 5)
-    list(src = outfile,
+    rppa_line_outfile <- file.path(user_dir,"pngs",paste(user_id,"-","TCGA_rppa_network_profile.png",sep=""))
+    
+    ggsave(rppa_line_outfile,p,device ="png",width =4, height = rppa_line_height)
+    list(src = rppa_line_outfile,
          contentType = 'image/png',
          # width = "100%" ,
          # height = 900,
          alt = "This is alternate text")
-  }, deleteFile = TRUE)
+  }, deleteFile = FALSE)
   
   # callModule(rppa_line_contact,"rppa_rela",seg=plot_seg,cancer=cancer.text,gene=gene.text,pathway=path.text,title="Relation network between gene and cancer related pathways.")
 })
