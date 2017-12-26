a74280a7800bdbbae28b301cb64cd79aa1b6290d
Huffyphenix <949985347@qq.com>
2017-12-26 20:34:48 +0800

    commit to avoid a git pull option

--- server.R
+++ server.R
@@ -44,7 +44,7 @@ shinyServer(
     source(file = file.path(config$server, "welcome_server.R"), local = TRUE)
     
     # tcga expr
-    source(file = file.path(config$server, "tcga_expr_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "tcga_expr_server.R"), local = TRUE)
     
     # tcga cnv ----
 
@@ -54,16 +54,19 @@ shinyServer(
     # source(file = file.path(config$server, "tcga_snv_server.R"), local = TRUE)
     
     # tcga meth ----
-    # source(file = file.path(config$server, "tcga_meth_server.R"), local = TRUE)
+    source(file = file.path(config$server, "tcga_meth_server.R"), local = TRUE)
     
     # tcga mirna ----
     # source(file = file.path(config$server, "tcga_mirna_server.R"), local = TRUE)
     
+    # tcga rppa ----
+    source(file = file.path(config$server, "tcga_rppa_server.R"), local = TRUE)
+    
     # drug ----
     # gdsc
-    source(file = file.path(config$server, "tcga_gdsc_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "tcga_gdsc_server.R"), local = TRUE)
     # ctrp
-    source(file = file.path(config$server, "tcga_ctrp_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "tcga_ctrp_server.R"), local = TRUE)
     
     # Load data
     
