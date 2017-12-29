0671dd404cba278923a2934f70e5e8dbdfd8b3be
Huffyphenix <949985347@qq.com>
2017-12-29 13:27:53 +0800

    ui.R GETx_exp_ui repeat load and GTEx_eqtl_ui annotated

--- ui.R
+++ ui.R
@@ -149,8 +149,8 @@ body <- dashboardBody(
     # GTEx ----
 
     source(file = file.path(config$wd, "ui", "GTEx_exp_ui.R"), local =TRUE)$value,
-    source(file = file.path(config$wd, "ui", "GTEx_eqtl_ui.R"), local =TRUE)$value,
-    source(file = file.path(config$wd, "ui", "GTEx_exp_ui.R"), local = TRUE)$value,
+    # source(file = file.path(config$wd, "ui", "GTEx_eqtl_ui.R"), local =TRUE)$value,
+    # source(file = file.path(config$wd, "ui", "GTEx_exp_ui.R"), local = TRUE)$value,
 
     # TCGA ----
     # expr ----
