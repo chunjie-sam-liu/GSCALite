eded36bf04fb2adf01e6728dbf2b552b005b4940
Huffyphenix <949985347@qq.com>
2017-12-31 17:23:12 +0800

    commit to aviod pull error

--- server.R
+++ server.R
@@ -47,29 +47,29 @@ shinyServer(
     
     # tcga expr
 
-    source(file = file.path(config$server, "tcga_expr_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "tcga_expr_server.R"), local = TRUE)
 
 
     
     # tcga cnv ----
 
-    source(file = file.path(config$server, "tcga_cnv_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "tcga_cnv_server.R"), local = TRUE)
     
     #tcga snv ----
     source(file = file.path(config$server, "tcga_snv_server.R"), local = TRUE)
     
     # tcga meth ----
-   source(file = file.path(config$server, "tcga_meth_server.R"), local = TRUE)
+   # source(file = file.path(config$server, "tcga_meth_server.R"), local = TRUE)
     
     # tcga mirna ----
 
-    source(file = file.path(config$server, "tcga_mirna_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "tcga_mirna_server.R"), local = TRUE)
 
 
 
     
     # tcga rppa ----
-   source(file = file.path(config$server, "tcga_rppa_server.R"), local = TRUE)
+   # source(file = file.path(config$server, "tcga_rppa_server.R"), local = TRUE)
     
     # drug ----
     # gdsc
@@ -78,9 +78,9 @@ shinyServer(
 #    source(file = file.path(config$server, "tcga_ctrp_server.R"), local = TRUE)
     # GTEx
 
-    source(file = file.path(config$server, "GTEx_exp_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "GTEx_exp_server.R"), local = TRUE)
 
-    source(file = file.path(config$server, "GTEx_eqtl_server.R"), local = TRUE)
+    # source(file = file.path(config$server, "GTEx_eqtl_server.R"), local = TRUE)
 
     
   }
