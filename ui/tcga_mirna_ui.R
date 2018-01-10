4b9c12a33401c69f2a617be3fca309c00706a618
Huffyphenix <949985347@qq.com>
2018-01-10 14:21:09 +0800

    mirna networkD3 loader

--- ui/tcga_mirna_ui.R
+++ ui/tcga_mirna_ui.R
@@ -41,7 +41,7 @@ tabItem(
                  offset = 1,
                  shinydashboard::tabBox(id = "mirna_PLOT",title = "PLOT",width = 12,
                                         tabPanel(title="networkD3",
-                                                 forceNetworkOutput("mirna_net1",height = "700px")),
+                                                 forceNetworkOutput("mirna_net1",height = "700px") %>% withSpinner(color="#0dc5c1")),
                                         tabPanel(title= "visNetwork",
                                                  visNetwork::visNetworkOutput("mirna_net2",height = "700px") %>% withSpinner(color="#0dc5c1"))
                  )
