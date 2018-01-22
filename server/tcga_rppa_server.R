04c99aae965bfb351adef56410a1d11b9a55dbde
Huffyphenix <949985347@qq.com>
2018-01-22 21:05:12 +0800

    rppa part change to fit global

--- server/tcga_rppa_server.R
+++ server/tcga_rppa_server.R
@@ -3,68 +3,34 @@
 # server elements 'tcga_rppa' sub tab of 'tcga' tab
 
 
-# load rppa data ---------------------------------------------------------
-# diff methylation between tumor and normal
-print(glue::glue("{paste0(rep('-', 10), collapse = '')} Start loading rppa percent data @ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
-rppa_per <- readr::read_rds(file.path(config$database, "TCGA", "rppa", "pan32_gene_activate.inhibit_pathway_percent.rds.gz"))
-print(glue::glue("{paste0(rep('-', 10), collapse = '')} End loading rppa percent data @ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
-# -------------------------------------------------------- #
-print(glue::glue("{paste0(rep('-', 10), collapse = '')} Start loading rppa regulate data @ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
-rppa_relation <- readr::read_rds(file.path(config$database, "TCGA", "rppa", "pan32_gene_A-I-N_sig_pval_class.siplification.rds.gz"))
-print(glue::glue("{paste0(rep('-', 10), collapse = '')} End loading rppa regulate data @ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
+source(file.path(config$wd, "functions", "tcga_rppa_function.R"))
 
-#  get cancer type --------------------------------------------------------
-
-rppa_cancer_type <- callModule(cancerType, "rppa")
 
 # Cancer types value box selection ----------------------------------------
 
-callModule(module = cancerTypesSelect, id = "rppa", .sctps = rppa_cancer_type)
+callModule(module = cancerTypesSelect, id = "rppa", .sctps = input$select_ctps)
 # Check box ---------------------------------------------------------------
 
 callModule(module = selectAndAnalysis, id = "rppa", .id = "rppa")
 
-# button control --------------------------------------------------------
+# generate rppa result out ui -------------------------------------------------------
 
-# submit cancer type -------------------------------------------------------
-
-rppa_submit_analysis <- function(input, output, session) {
-  observeEvent(input$submit, {
-    status$rppa_submit <- TRUE
-    print(status$rppa_submit)
-  })
-}
-
-callModule(rppa_submit_analysis, "rppa")
-
-
-# monitor for gene list change
-rppa_gene_list <- eventReactive(
-  eventExpr = status$analysis,
-  ignoreNULL = TRUE,
-  valueExpr = {
-    # be sure the following code run after start analysis
-    if (status$analysis == TRUE) {
-      status$rppa_submit <- TRUE
-      shinyjs::disable(id = "rppa-submit")
-      as.character(gene_set$match)
-    }
-  }
-)
+output$ui_rppa_result <- shiny::renderUI({
+  fn_rppa_result(selected_analysis$rppa)
+})
 
 # analysis core -----------------------------------------------------------
 
-
-
-
 rppa_analysis <- eventReactive(
   {
-    status$rppa_submit == TRUE
+    status$analysis == TRUE
   },
   ignoreNULL = TRUE,
   valueExpr = {
-    if (status$rppa_submit == TRUE) {
-      if (length(rppa_gene_list()) != 0) {
+    if (status$analysis == TRUE) {
+      if (selected_analysis$rppa == TRUE) {
+        load_data_rppa()
+      if (length(gene_set$match) != 0) {
         shinyBS::createAlert(
           session = session, anchorId = "rppa-no_gene_set", title = "Information", style = "info",
           content = "Need a few minutes to draw picture, please wait.", append = FALSE
@@ -75,19 +41,67 @@ rppa_analysis <- eventReactive(
         # output$rppa_rela_plot <- renderImage({})
 
         print("-------------------------------RPPA part------------------------------------")
-        print(rppa_gene_list())
-        print(rppa_cancer_type())
-        print(glue::glue("{paste0(rep('-', 10), collapse = '')} start rppa relation analysis part@ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
-
-        # data processing ---------------------------------------------------------
-
-        rppa_relation %>%
-          dplyr::filter(cancer_types %in% rppa_cancer_type()) %>%
-          dplyr::mutate(data = purrr::map(data, filter_gene_list, gene_list = rppa_gene_list())) %>%
-          tidyr::unnest() -> gene_list_cancer_rppa_rela
-
+        print(gene_set$match)
+        print(selected_ctyps())
+        
         # ploting -----------------------------------------------------------------
-
+        # global plot -----------------------------------------------
+        print(glue::glue("{paste0(rep('-', 10), collapse = '')} Start rppa global analysis part@ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
+        
+        # get gene set /cancer type data ----
+        rppa_per %>%
+          dplyr::filter(symbol %in% gene_set$match) -> gene_list_rppa_per
+        
+        # rppa global pie plot----
+        gene_list_rppa_per %>%
+          tidyr::unnest() %>%
+          tidyr::gather(-symbol, -pathway, key = "class", value = "per") %>%
+          dplyr::mutate(class = plyr::revalue(class, replace = c("a" = "Activation", "i" = "Inhibition", "n" = "None"))) -> rppa_pie_plot_ready
+        
+        # arugument for plot
+        rppa_pie_height <- gene_set$match %>% length() * 0.25
+        if (rppa_pie_height > 15) {
+          rppa_pie_height <- 15
+        }
+        if (rppa_pie_height < 3) {
+          rppa_pie_height <- 3
+        }
+        rppa_pie_outfile <- file.path(user_dir, "pngs", paste(user_id, "-", "TCGA_rppa_pie_profile.png", sep = ""))
+        
+        # draw ----
+        callModule(rppaPiePlot, "rppa_pie", data = rppa_pie_plot_ready, y = "per", fill = "class", facet_grid = " symbol~pathway", height = rppa_pie_height, outfile = rppa_pie_outfile,status)
+        
+        # rppa global percentage ----
+        # data process
+        gene_list_rppa_per %>%
+          tidyr::unnest() %>%
+          dplyr::filter(a + i > 5 / 32) %>%
+          dplyr::select(-n) %>%
+          tidyr::gather(-symbol, -pathway, key = "class", value = "per") %>%
+          dplyr::mutate(per = ifelse(class == "i", -per * 100, per * 100)) %>%
+          tidyr::unite(pathway, c(pathway, class)) -> rppa_per_ready
+        
+        # pic draw
+        rppa_heat_height <- gene_set$match %>% length() * 0.1
+        if (rppa_heat_height > 15) {
+          rppa_heat_height <- 15
+        }
+        if (rppa_heat_height < 3) {
+          rppa_heat_height <- 3
+        }
+        rppa_heat_outfile <- file.path(user_dir, "pngs", paste(user_id, "-", "TCGA_rppa_heatmap_percentage.png", sep = ""))
+        callModule(rppa_heat_per, "rppa_per", rppa_per_ready = rppa_per_ready, pathway = "pathway", symbol = "symbol", per = "per", height = rppa_heat_height, outfile = rppa_heat_outfile,status)
+        print(glue::glue("{paste0(rep('-', 10), collapse = '')} Start rppa global analysis part@ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
+        
+        # rppa relation plot ---------------------------------------------------
+        print(glue::glue("{paste0(rep('-', 10), collapse = '')} start rppa analysis part@ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
+        
+        # data processing
+                rppa_relation %>%
+          dplyr::filter(cancer_types %in% selected_ctyps()) %>%
+          dplyr::mutate(data = purrr::map(data, filter_gene_list, gene_list = gene_set$match)) %>%
+          tidyr::unnest() -> gene_list_cancer_rppa_rela
+                
         # rppa line contact ----
         # get data
         cancer_text <- get_rppa_text(gene_list_cancer_rppa_rela)
@@ -106,7 +120,7 @@ rppa_analysis <- eventReactive(
           dplyr::filter(type == "gene") -> gene.text
         cancer_text %>%
           dplyr::filter(type == "pathway") -> path.text
-        rppa_line_height <- rppa_gene_list() %>% length() * 0.1
+        rppa_line_height <- gene_set$match %>% length() * 0.1
         if (rppa_line_height > 15) {
           rppa_line_height <- 15
         }
@@ -115,7 +129,7 @@ rppa_analysis <- eventReactive(
         }
         # plot draw
         output$rppa_rela_plot <- renderImage({
-          status[["rppa_submit"]]
+          status[["analysis"]]
           ggplot() -> p
           for (cancers in plot_seg$Cancer %>% unique()) {
             # cancers="LUSC"
@@ -190,74 +204,13 @@ rppa_analysis <- eventReactive(
           session = session, anchorId = "rppa-no_gene_set", title = "Information", style = "info",
           content = .msg, append = FALSE
         )
-        status$rppa_submit <- FALSE
-        shinyjs::enable("rppa-submit")
       } else {
         shinyBS::createAlert(
           session = session, anchorId = "rppa-no_gene_set", title = "Oops",
           content = "No input gene set! Please go to Welcome page to input gene set.", style = "danger", append = FALSE
         )
       }
-    }
-  }
-)
-
-
-# analysis core -----------------------------------------------------------
-# submit cancer type -------------------------------------------------------
-rppa_global_analysis <- eventReactive(
-  {
-    status$analysis == TRUE
-  },
-  ignoreNULL = TRUE,
-  valueExpr = {
-    if (status$analysis == TRUE) {
-      print(glue::glue("{paste0(rep('-', 10), collapse = '')} Start rppa global analysis part@ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
-
-      # get gene set /cancer type data ----
-      rppa_per %>%
-        dplyr::filter(symbol %in% rppa_gene_list()) -> gene_list_rppa_per
-
-      # rppa global pie plot----
-      gene_list_rppa_per %>%
-        tidyr::unnest() %>%
-        tidyr::gather(-symbol, -pathway, key = "class", value = "per") %>%
-        dplyr::mutate(class = plyr::revalue(class, replace = c("a" = "Activation", "i" = "Inhibition", "n" = "None"))) -> rppa_pie_plot_ready
-
-      # arugument for plot
-      rppa_pie_height <- rppa_gene_list() %>% length() * 0.25
-      if (rppa_pie_height > 15) {
-        rppa_pie_height <- 15
       }
-      if (rppa_pie_height < 3) {
-        rppa_pie_height <- 3
-      }
-      rppa_pie_outfile <- file.path(user_dir, "pngs", paste(user_id, "-", "TCGA_rppa_pie_profile.png", sep = ""))
-
-      # draw ----
-      callModule(rppaPiePlot, "rppa_pie", data = rppa_pie_plot_ready, y = "per", fill = "class", facet_grid = " symbol~pathway", height = rppa_pie_height, outfile = rppa_pie_outfile,status)
-
-      # rppa global percentage ----
-      # data process
-      gene_list_rppa_per %>%
-        tidyr::unnest() %>%
-        dplyr::filter(a + i > 5 / 32) %>%
-        dplyr::select(-n) %>%
-        tidyr::gather(-symbol, -pathway, key = "class", value = "per") %>%
-        dplyr::mutate(per = ifelse(class == "i", -per * 100, per * 100)) %>%
-        tidyr::unite(pathway, c(pathway, class)) -> rppa_per_ready
-
-      # pic draw
-      rppa_heat_height <- rppa_gene_list() %>% length() * 0.1
-      if (rppa_heat_height > 15) {
-        rppa_heat_height <- 15
-      }
-      if (rppa_heat_height < 3) {
-        rppa_heat_height <- 3
-      }
-      rppa_heat_outfile <- file.path(user_dir, "pngs", paste(user_id, "-", "TCGA_rppa_heatmap_percentage.png", sep = ""))
-      callModule(rppa_heat_per, "rppa_per", rppa_per_ready = rppa_per_ready, pathway = "pathway", symbol = "symbol", per = "per", height = rppa_heat_height, outfile = rppa_heat_outfile,status)
-      print(glue::glue("{paste0(rep('-', 10), collapse = '')} Start rppa global analysis part@ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
     }
   }
 )
@@ -266,6 +219,4 @@ rppa_global_analysis <- eventReactive(
 
 # monitor -----------------------------------------------------------------
 
-observe(rppa_gene_list())
 observe(rppa_analysis())
-observe(rppa_global_analysis())
