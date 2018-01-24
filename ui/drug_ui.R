e9907c06e231730025505fbd96e57ca2e920d56c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-01-24 15:54:32 +0800

    drug help info

--- ui/drug_ui.R
+++ ui/drug_ui.R
@@ -5,10 +5,10 @@ tabItem(
   
   fluidRow(style = "width:80%;", shiny::uiOutput(outputId = "ui_drug_welcome")),
   
+  fluidRow(style = "width:80%", shiny::uiOutput(outputId = "ui_drug_help")),
+  
   fluidRow(shiny::uiOutput(outputId = "ui_drug_result")),
-  # fluidRow(drugOutput("drug")),
-  
-  
+
   # Load footer ----
   source(file.path(config$wd, "ui", "footer.R"), echo = FALSE, verbose = FALSE)$value
   
