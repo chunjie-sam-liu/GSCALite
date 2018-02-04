cc0364fae5904aaa49c33d26ddade1527bc8ede5
Huffyphenix <949985347@qq.com>
2018-02-04 19:57:42 +0800

    methy part message optimization

--- server/tcga_meth_server.R
+++ server/tcga_meth_server.R
@@ -74,8 +74,9 @@ meth_analysis <- eventReactive(
               dplyr::arrange(rank) -> cancer_rank.methdiff
 
             callModule(methy_diff_pointPlot, "meth_diff", data = gene_list_cancer_methdiff, cancer = "cancer_types", gene = "symbol", size = "fdr", color = "diff", cancer_rank = cancer_rank.methdiff, gene_rank = gene_rank.methdiff, sizename = "-Log10(FDR)", colorname = "Methylation diff (T - N)", title = "Methylation difference between tumor and normal samples.", status_monitor = "analysis", status, downloadname="Differential_methylation")
+            .msg_meth_diff <- NULL
           } else {
-            .msg <- paste(.msg, glue::glue("The [Differential Methylation] analysis based on paired sample in each cancer types.
+            .msg_meth_diff <- paste(glue::glue("The [Differential Methylation] analysis based on paired sample in each cancer types.
 In this analysis, only {nrow(meth_diff)} cancer types have paired samples. They are {paste0(meth_diff$cancer_types, collapse = ', ')}."), sep = " ")
           }
 
@@ -98,8 +99,9 @@ In this analysis, only {nrow(meth_diff)} cancer types have paired samples. They 
               dplyr::arrange(rank) -> cancer_rank.methsur
 
             callModule(snv_sur_pointPlot, "meth_survival", data = gene_list_cancer_methsur, cancer = "cancer_types", gene = "symbol", size = "log10logrankP", color = "Hyper_worse", cancer_rank = cancer_rank.methsur, gene_rank = gene_rank.methsur, sizename = "logRank Pvalue", colorname = "HyperMethy Worse", title = "Overall survival difference between hypermethylation and hypomethylation.", status_monitor = "analysis", status, downloadname="Methylation_survival")
+            .msg_meth_survival <- NULL
           } else {
-            .msg <- paste(.msg, glue::glue("No significant [Methylation Survival] result of gene: {paste0(gene_set$match, collapse = ', ')} in your selected cancer type: {paste0(selected_ctyps(), collapse = ', ')}."), sep = " ")
+            .msg_meth_survival <- paste(.msg, glue::glue("No significant [Methylation Survival] result of gene: {paste0(gene_set$match, collapse = ', ')} in your selected cancer type: {paste0(selected_ctyps(), collapse = ', ')}. Please try more cancers or more genes."), sep = " ")
             output[["meth_survival-plot"]] <- renderPlot({
               NULL
             })
@@ -122,13 +124,31 @@ In this analysis, only {nrow(meth_diff)} cancer types have paired samples. They 
               dplyr::arrange(rank) -> cancer_rank.methcor
 
             callModule(methy_diff_pointPlot, "meth_exp", data = gene_list_cancer_methcor, cancer = "cancer_types", gene = "symbol", size = "logfdr", color = "spm", cancer_rank = cancer_rank.methcor, gene_rank = gene_rank.methcor, sizename = "-Log10(P.value)", colorname = "Spearman Correlation Coefficient", title = "Spearman Correlation Coefficient of methylation and gene expression.", status_monitor = "analysis", status, downloadname="Methylation_affect_exp")
+            .msg_meth_exp <- NULL
           } else {
-            msg <- paste(.msg, glue::glue("No significant [Methylation to Expression] result of gene: {paste0(gene_set$match, collapse = ', ')} in your selected cancer type: {paste0(selected_ctyps(), collapse = ', ')}."), sep = " ")
+            .msg_meth_exp <- paste(.msg, glue::glue("No significant [Methylation to Expression] result of gene: {paste0(gene_set$match, collapse = ', ')} in your selected cancer type: {paste0(selected_ctyps(), collapse = ', ')}. Please try more cancers or more genes."), sep = " ")
             output[["meth_exp-plot"]] <- renderPlot({
               NULL
             })
           }
           print(glue::glue("{paste0(rep('-', 10), collapse = '')} End methy part analysis @ {Sys.time()} {paste0(rep('-', 10), collapse = '')}"))
+          
+          # infomation UI for each part --------------------------
+          output[["meth_diff-massage"]] <- renderUI({
+            tagList(
+              shiny::tags$p(.msg_meth_diff,style= "color:#CD3700")
+            )
+          })
+          output[["meth_survival-massage"]] <- renderUI({
+            tagList(
+              shiny::tags$p(.msg_meth_survival,style= "color:#CD3700")
+            )
+          })
+          output[["meth_exp-massage"]] <- renderUI({
+            tagList(
+              shiny::tags$p(.msg_meth_exp,style= "color:#CD3700")
+            )
+          })
 
           .msg <- paste(.msg, glue::glue("Since we just show significant results, so a small size of gene and cancer set may cause no significant result in some plots, if it happens, try more genes and cancer types."), sep = " ")
           # alert for information
