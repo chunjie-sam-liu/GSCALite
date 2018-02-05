ccb86597a53afdc31f40cb10a52ae42dd433aa12
Huffyphenix <949985347@qq.com>
2018-02-05 17:15:05 +0800

    snv pic width change to fit page

--- functions/tcga_snv_function.R
+++ functions/tcga_snv_function.R
@@ -6,8 +6,8 @@ snvOutput <- function() {
       id = "snv_plot", title = "", width = 12,
       # bubble plot for tumor vs. normal
       tabPanel(title= "SNV percentage profile",PlotInput(id="snv_percentage")),
-      tabPanel(title="SNV summary",imagePlotInput("snv_summary",width=700,height="100%")),
-      tabPanel(title="SNV oncoplot",imagePlotInput("snv_oncoplot",width=700,height="100%")),
+      tabPanel(title="SNV summary",imagePlotInput("snv_summary",width="100%",height="100%")),
+      tabPanel(title="SNV oncoplot",imagePlotInput("snv_oncoplot",width="100%",height="100%")),
       tabPanel(title="SNV survival",PlotInput("snv_survival"))
     )
   )
