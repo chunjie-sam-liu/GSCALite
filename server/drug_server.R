4faef5f28dffb467368e43922e1ac5bf88a018ef
Huffyphenix <949985347@qq.com>
2018-02-05 16:38:18 +0800

    drug server massage added

--- server/drug_server.R
+++ server/drug_server.R
@@ -14,32 +14,56 @@ output$ui_drug_help <- shiny::renderUI({fn_drug_help()})
 
 output$ui_drug_result <- shiny::renderUI({fn_drug_result(selected_analysis$drug)})
 
-drug_output <- function(input, output, session, .path, .gs){
+drug_output <- function(input, output, session, ctrp_gene_list_sig_drug, gdsc_gene_list_sig_drug, t_gdsc){
   output$gdsc <- renderPlot({NULL})
   output$ctrp <- renderPlot({NULL})
   # GDSC 
-  output$gdsc <- renderPlot(expr = {gdsc_plot(.path, .gs)})
-  output$`gdsc-picdownload` <- downloadHandler(
-    filename = function() {
-      paste("GDSC_drug_sensitivity", ".", input$`gdsc-pictype`, sep = "")
-    },
-    content = function(file){
-      print(file)
-      ggsave(file,gdsc_plot(.path, .gs),device = input$`gdsc-pictype`,width = input$`gdsc-d_width`,height = input$`gdsc-d_height`)
-    }
-  )
+  if(nrow(gdsc_gene_list_sig_drug)>0){
+    output$gdsc <- renderPlot(expr = {gdsc_plot(gdsc_gene_list_sig_drug, t_gdsc)})
+    output$`gdsc-picdownload` <- downloadHandler(
+      filename = function() {
+        paste("GDSC_drug_sensitivity", ".", input$`gdsc-pictype`, sep = "")
+      },
+      content = function(file){
+        print(file)
+        ggsave(file,gdsc_plot(gdsc_gene_list_sig_drug, t_gdsc),device = input$`gdsc-pictype`,width = input$`gdsc-d_width`,height = input$`gdsc-d_height`)
+      }
+    )
+    .msg_gdsc <- NULL
+    } else {
+  .msg_gdsc <- paste(glue::glue("Your selected genes: {paste0(gene_set$match, collapse = ', ')} have on correlation with GDSC drug sensitivity."), sep = " ")
+  }
+  
   
   #CTRP
-  output$ctrp <- renderPlot(expr = {ctrp_plot(.path, .gs)})
+  if(nrow(ctrp_gene_list_sig_drug)>0){
+  output$ctrp <- renderPlot(expr = {ctrp_plot(ctrp_gene_list_sig_drug)})
   output$`ctrp-picdownload` <- downloadHandler(
     filename = function() {
       paste("CTRP_drug_sensitivity", ".", input$`ctrp-pictype`, sep = "")
     },
     content = function(file){
       print(file)
-      ggsave(file,ctrp_plot(.path, .gs),device = input$`ctrp-pictype`,width = input$`ctrp-d_width`,height = input$`ctrp-d_height`)
+      ggsave(file,ctrp_plot(ctrp_gene_list_sig_drug),device = input$`ctrp-pictype`,width = input$`ctrp-d_width`,height = input$`ctrp-d_height`)
     }
   )
+  .msg_ctrp <- NULL
+  } else {
+    .msg_ctrp <- paste(glue::glue("Your selected genes: {paste0(gene_set$match, collapse = ', ')} have on correlation with CTRP drug sensitivity."), sep = " ")
+  }
+  
+  # message output ----
+  output[["gdsc_message"]] <- renderUI({
+    tagList(
+      shiny::tags$p(.msg_gdsc, style = "color:#CD3700")
+    )
+  })
+  output[["ctrp_message"]] <- renderUI({
+    tagList(
+      shiny::tags$p(.msg_ctrp, style = "color:#CD3700")
+    )
+  })
+  
 }
 
 drug_analysis <- eventReactive(
@@ -47,7 +71,19 @@ drug_analysis <- eventReactive(
   ignoreNULL = TRUE,
   valueExpr = {
     if (status$analysis == TRUE && selected_analysis$drug == TRUE) {
-      callModule(module = drug_output, id = "drug", .path = config$database, .gs = gene_set$match)
+      load_data_drug()
+      drug_gdsc %>%
+        dplyr::filter(symbol %in% gene_set$match) %>%
+        dplyr::mutate(cor_drug = purrr::map(.x = drug, .f = fn_filter_drug)) %>%
+        tidyr::unnest(cor_drug) -> gdsc_gene_list_sig_drug
+      
+      
+      drug_ctrp %>%
+        dplyr::filter(symbol %in% gene_set$match) %>%
+        dplyr::mutate(cor_drug = purrr::map(.x = drug, .f = fn_filter_drug_ctrp)) %>%
+        tidyr::unnest(cor_drug) -> ctrp_gene_list_sig_drug
+      
+      callModule(module = drug_output, id = "drug",  ctrp_gene_list_sig_drug, gdsc_gene_list_sig_drug, t_gdsc)
       
     }
   }
