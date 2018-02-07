e7fea19b50698f0a7550668e8681aca670606f4b
Huffyphenix <949985347@qq.com>
2018-02-07 08:20:12 +0800

    delete cnv bar help

--- ui/tcga_cnv_help.R
+++ ui/tcga_cnv_help.R
@@ -66,10 +66,10 @@ fluidRow(
                     shiny::tags$td("Homo CNV"),
                     shiny::tags$td("Homozygous CNV profile show you percentage of homozygous CNV, include the amplification and deletion percentage of homozygous CNV about each gene in each cancer.")
                   ),
-                  shiny::tags$tr(
-                    shiny::tags$td("Overall CNV Frequency"),
-                    shiny::tags$td("Overall CNV frenquency gives you a bar plot, which shows the sum frequency of all genes' CNV in each cancer type. For example, you searched 10 genes, red bar in Hete CNV: [Type Amp]=number(samples with any one of these genes have hete CNV amplification)/number(all samples), means this part of samples have at least one gene contains heterozygous amplification, but also may have genes contain hetero/homozygous deletion; wathet blue bar in Hete CNV: [Type Amp Only]=number(samples with any one of these genes only have hete CNV amplification)/number(all samples), means this part of samples have at least one gene contains only hete CNV amplification. So the sum of them may greater than 1.")
-                  ),
+                  # shiny::tags$tr(
+                  #   shiny::tags$td("Overall CNV Frequency"),
+                  #   shiny::tags$td("Overall CNV frenquency gives you a bar plot, which shows the sum frequency of all genes' CNV in each cancer type. For example, you searched 10 genes, red bar in Hete CNV: [Type Amp]=number(samples with any one of these genes have hete CNV amplification)/number(all samples), means this part of samples have at least one gene contains heterozygous amplification, but also may have genes contain hetero/homozygous deletion; wathet blue bar in Hete CNV: [Type Amp Only]=number(samples with any one of these genes only have hete CNV amplification)/number(all samples), means this part of samples have at least one gene contains only hete CNV amplification. So the sum of them may greater than 1.")
+                  # ),
                   shiny::tags$tr(
                     shiny::tags$td("CNV correlate to gene expression"),
                     shiny::tags$td("CNV can influence the expression of gene in theory. Use person correlation to get the relationship between gene expression and cnv, gene with FDR<=0.05 will be remained. From this, we can get genes whose expression significantly regulated by CNV. Blue bubbles represent a negative correlation (means when gene having a high frequency of CNV, gene's expression downregulate, they have opposite trend), and red bubbles represent positive correlation (means when gene having a high frequency of CNV, the gene expression upregulate too, they have consistent trend), the deeper of color, the higher the correlation. And size of the point represents statistic significance, the bigger of size, the more statistic significant.")
