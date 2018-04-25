bc972a10df8e21ab2dc0231c0f470c07a1be5927
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-04-25 09:55:33 +0800

    add address

--- ui/contact_ui.R
+++ ui/contact_ui.R
@@ -8,16 +8,40 @@ tabItem(
   
   # Welcome message ----
   fluidRow(
-    style = "width:95%;",
+    style = "width:80%;", 
     
     column(
-      width = 12, offset = 0,
-      shiny::tags$img(
-        src = "./imgs/about.png",
-        class = "center-block img-responsive",
-        style = "height: 600px;"
+      width = 12, offset = 0, align = "left",
+      
+      shiny::tags$h1(
+        class = "text-success text-left",
+        shiny::icon(name = "angle-double-right", class = "fa-fw"),
+        "Contact"
       ),
-      shiny::tags$h1("Main authors contribute to this work.")
+      shiny::hr(),
+      
+      shiny::tags$address(
+        shiny::tags$p(shiny::tags$strong("An-Yuan Guo, Ph.D. Professor of Bioinformatics")),
+        shiny::tags$p(shiny::tags$strong("Email: guoay@hust.edu.cn"))
+        ),
+      shiny::tags$address(
+        shiny::tags$p(shiny::tags$strong("Chun-Jie Liu, Ph.D. Candidate")),
+        shiny::tags$p(shiny::tags$strong("Email: samliu@hust.edu.cn"))
+      ),
+      shiny::tags$address(
+        shiny::tags$p(shiny::tags$strong("Fei-Fei LHu, Ph.D. Candidate")),
+        shiny::tags$p(shiny::tags$strong("Email: hufeifei@hust.edu.cn"))
+      ),
+      shiny::tags$address(
+        shiny::tags$p(shiny::tags$strong("Qiong Zhang, Postdoc Fellow")),
+        shiny::tags$p(shiny::tags$strong("Email: zhangqiong@hust.edu.cn"))
+      )
+      # shiny::tags$img(
+      #   src = "./imgs/about.png",
+      #   class = "center-block img-responsive",
+      #   style = "height: 600px;"
+      # ),
+      # shiny::tags$h1("Main authors contribute to this work.")
     )
   ),
   # Load footer ----
