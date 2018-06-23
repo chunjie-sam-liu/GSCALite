c163994613a5066b211dd9ca6b157366aa871159
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-06-23 14:01:11 +0800

    change the data directory

--- config.R
+++ config.R
@@ -30,7 +30,7 @@ config$wd <- "/home/liucj/github/GSCALite"
 
 # Database ----------------------------------------------------------------
 # This contains the TCGA, GTEx and Drug data
-config$database <- "/data/GSCALite"
+config$database <- "/data/shiny-data/GSCALite"
 
 # Path bins ---------------------------------------------------------------
 config$bins <- "bins"
